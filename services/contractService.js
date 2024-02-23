4e9a570ad2bdf06513da648401c66a6c39116397
NMilivojevic <nevmilivojevic@gmail.com>
2024-02-23 23:19:14 +0100

    parse

--- services/contractService.js
+++ services/contractService.js
@@ -9,7 +9,18 @@ const contract = new web3.eth.Contract(TestContract.abi, contractAddress);
 
 const getBalance = async (account) => {
     const balance = await contract.methods.balanceOf(account).call();
-    return (parseFloat(balance) / 10 ** 18).toLocaleString();
+    const parsedBalance = parseFloat(balance) / 10 ** 18;
+
+    // Define a tolerance for the comparison
+    const tolerance = 1e-10;
+
+    // Check if the absolute difference between the parsed balance and 1e-7 is within the tolerance
+    if (Math.abs(parsedBalance - 1e-7) < tolerance) {
+        return 0;
+    }
+
+    // Otherwise, return the parsed balance
+    return parsedBalance;
 };
 
 const getMinStakeAmount = async () => {
